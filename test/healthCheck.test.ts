536a9b2c853e0c5235f5d9fde1e3df14d13d8cae
Andrei Anoling <anoling.andrei@gmail.com>
2026-01-16 22:39:38 -0600

    Added portfolioperformance file

--- test/healthCheck.test.ts
+++ test/healthCheck.test.ts
@@ -4,6 +4,7 @@ import app from "../src/app";
 describe("GET /api/v1/routes", () => {
     it("should return server routes information", async () => {
         const response: Response = await request(app).get("/src/api/v1/routes");
+        expect(response.status).toBe(200);
         expect(response.body.status).toBe("OK");
         expect(response.body).toHaveProperty("version");
     });
